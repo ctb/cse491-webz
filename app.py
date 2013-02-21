7b5f07c7827ff8e38cbfe63987c9463d3012e9d1
C. Titus Brown <titus@idyll.org>
2013-02-21 14:25:35 -0500

    updated

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 from wsgiref.simple_server import make_server
+import urlparse
 
 class SimpleApp(object):
     def __call__(self, environ, start_response):
@@ -14,7 +15,10 @@ Visit:
 <a href='content'>a file</a>,
 <a href='error'>an error</a>,
 <a href='helmet'>an image</a>,
-<a href='somethingelse'>something else</a>
+<a href='somethingelse'>something else</a>, or
+<a href='form'>a form...</a>
+<p>
+<img src='/helmet'>
 """
         elif path == '/content':
             content_type = 'text/html'
@@ -26,6 +30,18 @@ Visit:
         elif path == '/helmet':
             content_type = 'image/gif'
             data = open('Spartan-helmet-Black-150-pxls.gif', 'rb').read()
+        elif path == '/form':
+            content_type = 'text/html'
+            data = form()
+        elif path == '/recv':
+            formdata = environ['QUERY_STRING']
+            results = urlparse.parse_qs(formdata)
+
+            firstname = results['firstname'][0]
+            lastname = results['lastname'][0]
+
+            content_type = 'text/html'
+            data = "First name: %s; last name: %s.  <a href='./'>return to index</a>" % (firstname, lastname)
         else:
             content_type = 'text/plain'
             data = "Hello, world; got path request %s" % environ['PATH_INFO']
@@ -34,6 +50,15 @@ Visit:
         start_response(status, headers)
 
         return [data]
+
+def form():
+    return """
+<form action='recv'>
+Your first name? <input type='text' name='firstname' size'20'>
+Your last name? <input type='text' name='lastname' size='20'>
+<input type='submit'>
+</form>
+"""
 
 if __name__ == '__main__':
     import random, socket
