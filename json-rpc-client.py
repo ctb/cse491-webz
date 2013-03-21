03cf210279325402d5ef10935f7d4a61f9838bb3
C. Titus Brown <titus@idyll.org>
2013-03-21 14:02:11 -0400

    refactored to make calls more generic

--- json-rpc-client.py
+++ json-rpc-client.py
@@ -3,10 +3,32 @@ import sys
 import simplejson
 import urllib2
 
-def call_remote(base, method, params, id):
-    # determine the URL to call
-    url = base + 'rpc'
+class MagicFunction(object):
+    def __init__(self, base, name):
+        self.url = base
+        self.name = name
 
+    def __call__(self, *params):
+        return call_remote(self.url, self.name, params, 1)
+
+class JSON_RPC_Magic(object):
+    def __init__(self, base_http_url):
+        self.url = base_http_url + 'rpc'
+        self.methods = set(['hello', 'add'])
+
+    def __getattr__(self, name):
+        if name in self.__dict__:
+            return self.__dict__[name]
+
+        if name not in self.__dict__['methods']:
+            return None
+
+        fn = MagicFunction(self.url, name)
+        self.__dict__[name] = fn
+
+        return fn
+
+def call_remote(base, method, params, id):
     # encode things in a dict that is then converted into JSON
     d = dict(method=method, params=params, id=id)
     encoded = simplejson.dumps(d)
@@ -15,7 +37,7 @@ def call_remote(base, method, params, id):
     headers = { 'Content-Type' : 'application/json' }
 
     # call remote server
-    req = urllib2.Request(url, encoded, headers)
+    req = urllib2.Request(base, encoded, headers)
 
     # get response
     response_stream = urllib2.urlopen(req)
@@ -28,8 +50,6 @@ def call_remote(base, method, params, id):
     return response['result']
 
 if __name__ == '__main__':
-    server_base = sys.argv[1]
-
-    print 'hello!', call_remote(server_base, method='hello', params=[], id=1)
-
-    print 'add:', call_remote(server_base, method='add', params=[1, 2], id=1)
+    magic = JSON_RPC_Magic(sys.argv[1])
+    print magic.hello()
+    print magic.add(2, 3)
